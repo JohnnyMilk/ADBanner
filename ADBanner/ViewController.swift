0a001e521a956e2ad6cbe37030ed18b8f93b98d9
Johnny Wang <johnny@doubleservice.com>
2017-10-03 15:22:25 +0800

    Initial Commit

---
+++ ADBanner/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  ADBanner
+//
+//  Created by Johnny Wang on 2017/9/29.
+//  Copyright © 2017年 Johnny Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, MLBannerViewBehaviorDelegate {
+    @IBOutlet weak var myBanner: MLBannerView!
+    
+    func whenPagingComplete(currentPage: Int, currentView: UIView) {
+        print("You paging to: \(currentPage)")
+    }
+    
+    func whenTapView(currentPage: Int, currentView: UIView) {
+        print("You are on tap: \(currentPage)")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //let myBanner = MLBannerView(frame: view.frame)
+        myBanner.setNumberOfPages(7)
+        myBanner.delegate = self
+        
+        for innerView in myBanner.bannerList {
+            let r = CGFloat((arc4random() % 10)) / 10.0
+            let g = CGFloat((arc4random() % 10)) / 10.0
+            let b = CGFloat((arc4random() % 10)) / 10.0
+            innerView.backgroundColor = UIColor.init(red: CGFloat(r), green: CGFloat(g), blue: CGFloat(b), alpha: 1.0)
+            
+            let label = UILabel(frame: CGRect(origin: CGPoint.zero, size: innerView.frame.size))
+            label.numberOfLines = 2
+            label.textAlignment = .center
+            label.font = UIFont(name: label.font.familyName, size: 30.0)
+            label.text = "R:\(CGFloat(r))  G:\(CGFloat(g))  B:\(CGFloat(b)) \n (Background Color)"
+            label.textColor = UIColor.white
+            
+            innerView.addSubview(label)
+        }
+        
+        view.addSubview(myBanner)
+        myBanner.isHiddenPageControl(false)
+        
+        myBanner.startAutoPaging(6.0)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MLBannerView.swift
@@ -0,0 +1,153 @@
+//
+//  MLBannerView.swift
+//  ADBanner
+//
+//  Created by Johnny Wang on 2017/9/29.
+//  Copyright © 2017年 Johnny Wang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol MLBannerViewBehaviorDelegate {
+    func whenPagingComplete(currentPage: Int, currentView: UIView)
+    func whenTapView(currentPage: Int, currentView: UIView)
+}
+
+// Let keeps protocol optional.
+extension MLBannerViewBehaviorDelegate {
+    func whenPagingComplete(currentPage: Int, currentView: UIView) {}
+}
+
+class MLBannerView: UIView, UIScrollViewDelegate {
+    private var scrollView: UIScrollView
+    private var pageControl: UIPageControl
+    private var timer: Timer
+    var bannerList: [UIView]
+    var delegate: MLBannerViewBehaviorDelegate?
+    
+    override init(frame: CGRect) {
+        scrollView = UIScrollView()
+        pageControl = UIPageControl()
+        timer = Timer()
+        bannerList = [UIView]()
+        super.init(frame: frame)
+    
+        addSubview(scrollView)
+        scrollViewSetting()
+        addSubview(pageControl)
+        pageControlSetting()
+        bannerListSetting()
+        tapGestureRecognizerSetting()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        scrollView = UIScrollView()
+        pageControl = UIPageControl()
+        timer = Timer()
+        bannerList = [UIView]()
+        super.init(coder: aDecoder)
+        
+        addSubview(scrollView)
+        scrollViewSetting()
+        addSubview(pageControl)
+        pageControlSetting()
+        bannerListSetting()
+        tapGestureRecognizerSetting()
+    }
+    
+    private func scrollViewSetting() {
+        scrollView.delegate = self
+        scrollView.frame = CGRect(x: 0.0, y: 0.0, width: frame.width, height: frame.height)
+        scrollView.scrollsToTop = false
+        scrollView.isPagingEnabled = true
+        scrollView.alwaysBounceVertical = false
+        scrollView.isDirectionalLockEnabled = true
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.isUserInteractionEnabled = true
+    }
+    
+    private func pageControlSetting() {
+        pageControl.currentPage = 0
+        pageControl.numberOfPages = 1
+        pageControl.translatesAutoresizingMaskIntoConstraints = false
+        pageControl.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        pageControl.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        
+        setNeedsLayout()
+    }
+    
+    private func bannerListSetting() {
+        bannerList.removeAll()
+        let width = scrollView.frame.size.width
+        let height = scrollView.frame.size.height
+        for page in 0...pageControl.numberOfPages - 1 {
+            let innerView = UIView(frame: CGRect(x: width*CGFloat(page), y: 0.0, width: width, height: height))
+            bannerList.append(innerView)
+            scrollView.addSubview(bannerList[page])
+        }
+        
+        scrollView.contentSize = CGSize(width: width*CGFloat(pageControl.numberOfPages), height: height)
+    }
+    
+    private func tapGestureRecognizerSetting() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.whenTap))
+        tapGestureRecognizer.numberOfTouchesRequired = 1
+        tapGestureRecognizer.numberOfTapsRequired = 1
+        self.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    func setNumberOfPages(_ pages: Int) {
+        removeAllSubviews()
+        pageControl.numberOfPages = pages
+        bannerListSetting()
+    }
+    
+    private func removeAllSubviews() {
+        for innerView in bannerList {
+            innerView.removeFromSuperview()
+        }
+    }
+    
+    @objc private func whenTap() {
+        let currentPage = pageControl.currentPage
+        delegate?.whenTapView(currentPage:currentPage, currentView: bannerList[currentPage])
+    }
+    
+    func startAutoPaging(_ second: Double) {
+        timer = Timer.scheduledTimer(timeInterval: second, target: self, selector: #selector(self.paging), userInfo: nil, repeats: true)
+    }
+    
+    func stopAutoPaging() {
+        timer.invalidate()
+    }
+    
+    @objc private func paging() {
+        let page = pageControl.currentPage
+        let currentPage = (page + 1 >= pageControl.numberOfPages) ? 0 : page + 1
+        
+        let width = scrollView.frame.size.width
+        let height = scrollView.frame.size.height
+        let destination = CGRect(x: width*CGFloat(currentPage), y: 0.0, width: width, height: height)
+        
+       scrollView.scrollRectToVisible(destination, animated: true)
+    }
+    
+    func isHiddenPageControl(_ b: Bool) {
+        pageControl.isHidden = b
+    }
+    
+    internal func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        scrollView.contentOffset = CGPoint(x: scrollView.contentOffset.x, y: 0.0)
+        
+        let width = scrollView.frame.size.width
+        let currentPage = Int(((scrollView.contentOffset.x - width / 2) / width) + 1)
+        pageControl.currentPage = currentPage
+        
+        if scrollView.contentOffset.x.truncatingRemainder(dividingBy: self.frame.width) == 0 {
+            delegate?.whenPagingComplete(currentPage:currentPage, currentView: bannerList[currentPage])
+        }
+    }
+
+}
